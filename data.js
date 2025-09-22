c34ef63fdf1e1f844367b30138ae4af3ee0c5a0a
OnePerec <koninas@gmail.com>
2025-09-22 13:12:06 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+// Укажи путь к панораме (JPG/PNG/WebP), формат должен быть 2:1 (например, 8000x4000)
+window.DATA = {
+  scene: {
+    title: "Тестовая сцена",
+    url: "panos/sample.jpg", // ← замени на свой файл, например "panos/school_entrance.webp"
+    width: 8000,
+    yaw: 0, pitch: 0, fov: 1.2
+  }
+};
